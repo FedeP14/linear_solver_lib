16609a3af0b547365cd05ee725f8603fe03fea19
Federico Pulcino <federico.pulcino@gmail.com>
2024-07-31 09:07:39 +0200

    refactor

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"gonum.org/v1/gonum/mat"
 
@@ -22,20 +21,16 @@ func main() {
 
 	matrix, err := utils.ReadMatrixFromFile(matrixPath + "/" + matrixFile)
 	if err != nil {
-		fmt.Println("Error reading the matrix", err)
+		fmt.Println("Error reading the matrix:", err)
 		return
 	}
 
-	fmt.Print("Matrix A size: " + fmt.Sprint(matrix.Dims()) + "\n")
+	r, c := matrix.Dims()
+	fmt.Printf("Matrix A size: %v x %v\n", r, c)
 	n, _ := matrix.Dims()
-	x_solution := mat.NewVecDense(n, nil)
-	for i := 0; i < n; i++ {
-		x_solution.SetVec(i, 1.0)
-	}
-	x_approx := mat.NewVecDense(n, nil)
-
-	b := mat.NewVecDense(n, nil)
-	b.MulVec(matrix, x_solution)
+	xSolution := utils.CreateSolutionVector(n)
+	xApprox := mat.NewVecDense(n, nil)
+	b := utils.CreateBVector(matrix, xSolution)
 
 	tol, err := menu.SelectTolerance(params.Tolerances)
 	if err != nil {
@@ -43,58 +38,8 @@ func main() {
 		return
 	}
 
-	// Jacobi Method
-	fmt.Println()
-	fmt.Println("Jacobi Method:")
-	start := time.Now()
-	x_jacobi, iterations, converged := solver.Jacobi(matrix, b, x_approx, tol, params.MaxIter)
-	elapsed := time.Since(start)
-	if converged {
-		fmt.Printf("Time taken: %v\n", elapsed)
-		fmt.Printf("Number of iterations: %d\n", iterations)
-		fmt.Printf("Relative error: %e\n", utils.CalculateRelativeError(x_jacobi, x_solution))
-	} else {
-		fmt.Println("Jacobi did not converge")
-	}
-
-	fmt.Println()
-	// Gauss-Seidel Method
-	fmt.Println("Gauss-Seidel Method:")
-	start = time.Now()
-	x_gs, iterations, converged := solver.GaussSeidel(matrix, b, x_approx, tol, params.MaxIter)
-	elapsed = time.Since(start)
-	if converged {
-		fmt.Printf("Time taken: %v\n", elapsed)
-		fmt.Printf("Number of iterations: %d\n", iterations)
-		fmt.Printf("Relative error: %e\n", utils.CalculateRelativeError(x_gs, x_solution))
-	} else {
-		fmt.Println("Gauss-Seidel did not converge")
-	}
-
-	fmt.Println()
-	fmt.Println("Gradient Method:")
-	start = time.Now()
-	x_gradient, iterations, converged := solver.GradientMethod(matrix, b, x_approx, tol, params.MaxIter)
-	elapsed = time.Since(start)
-	if converged {
-		fmt.Printf("Time taken: %v\n", elapsed)
-		fmt.Printf("Number of iterations: %d\n", iterations)
-		fmt.Printf("Relative error: %e\n", utils.CalculateRelativeError(x_gradient, x_solution))
-	} else {
-		fmt.Println("Gradient method did not converge")
-	}
-
-	fmt.Println()
-	// Conjugate Gradient Method
-	fmt.Println("Conjugate Gradient Method:")
-	start = time.Now()
-	x_cg, iterations, converged := solver.ConjugateGradient(matrix, b, x_approx, tol, params.MaxIter)
-	elapsed = time.Since(start)
-	if converged {
-		fmt.Printf("Time taken: %v\n", elapsed)
-		fmt.Printf("Number of iterations: %d\n", iterations)
-		fmt.Printf("Relative error: %e\n", utils.CalculateRelativeError(x_cg, x_solution))
-	} else {
-		fmt.Println("Conjugate Gradient method did not converge")
-	}
+	utils.RunSolver("Jacobi Method", solver.Jacobi, matrix, b, xApprox, xSolution, tol)
+	utils.RunSolver("Gauss-Seidel Method", solver.GaussSeidel, matrix, b, xApprox, xSolution, tol)
+	utils.RunSolver("Gradient Method", solver.GradientMethod, matrix, b, xApprox, xSolution, tol)
+	utils.RunSolver("Conjugate Gradient Method", solver.ConjugateGradient, matrix, b, xApprox, xSolution, tol)
 }
--- utils/solver_utils.go
+++ utils/solver_utils.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"fmt"
+	"linear_system_lib/params"
+	"time"
+
 	"gonum.org/v1/gonum/mat"
 )
 
@@ -19,4 +23,34 @@ func CalculateRelativeError(x *mat.VecDense, x_solution *mat.VecDense) float64 {
 	var diff mat.VecDense
 	diff.SubVec(x, x_solution)
 	return mat.Norm(&diff, 2) / mat.Norm(x_solution, 2)
+}
+
+func CreateSolutionVector(size int) *mat.VecDense {
+	xSolution := mat.NewVecDense(size, nil)
+	for i := 0; i < size; i++ {
+		xSolution.SetVec(i, 1.0)
+	}
+	return xSolution
+}
+
+func CreateBVector(matrix *mat.Dense, xSolution *mat.VecDense) *mat.VecDense {
+	n, _ := matrix.Dims()
+	b := mat.NewVecDense(n, nil)
+	b.MulVec(matrix, xSolution)
+	return b
+}
+
+func RunSolver(name string, solverFunc func(*mat.Dense, *mat.VecDense, *mat.VecDense, float64, int) (*mat.VecDense, int, bool), matrix *mat.Dense, b, xApprox, xSolution *mat.VecDense, tol float64) {
+	fmt.Println()
+	fmt.Println(name + ":")
+	start := time.Now()
+	xResult, iterations, converged := solverFunc(matrix, b, xApprox, tol, params.MaxIter)
+	elapsed := time.Since(start)
+	if converged {
+		fmt.Printf("Time taken: %v\n", elapsed)
+		fmt.Printf("Number of iterations: %d\n", iterations)
+		fmt.Printf("Relative error: %e\n", CalculateRelativeError(xResult, xSolution))
+	} else {
+		fmt.Println(name + " did not converge")
+	}
 }
